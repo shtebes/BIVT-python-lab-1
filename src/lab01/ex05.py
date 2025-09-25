64b097100af90289b43f481bbcf5f47731f25eb3
shtebes <ainap2305@gmail.com>
2025-09-25 14:38:45 +0300

    awwe

--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -1,1 +1,7 @@
-familiya, imya, otchestvo = input("ФИО: ").split()
+fio = input("ФИО: ")
+fio_no_probel = ' '.join(fio.split())
+words = fio_no_probel.split()
+a = []
+initial = ''.join([i[0].upper() for i in words])
+print(f"Инициалы: {initial}.")
+print(f"Длина (символов): {len(fio_no_probel)}")
